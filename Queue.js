3a17ff3d99a07097cd68f0496bdfdcd0726b7457
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-02-17 13:07:02 -0800

    More queue methods added

--- Queue.js
+++ Queue.js
@@ -6,8 +6,7 @@
 // To enqueue into a queue, simply use the method push. Enqueue method uses push
 // To dequeue from a queue, simply use the method shift. Dequeue method uses shift
 
-// Easy way to implement a queue
-
+// ======== Easy way to implement a queue ==========
 var queue = [];
 
 queue.push("A");
@@ -23,18 +22,46 @@ console.log(queue);
 queue.shift();
 console.log(queue);
 
-// Queue implementation using a class
+// ======== Queue implementation using a class ========
 class Queue {
-  constructor() {
+  constructor(size) {
     this.storage = [];
+    this.size = size;
+    this.pointer = 0;
   }
 
   enqueue(item) {
-    this.storage.push(item);
+    if (this.pointer < this.size) {
+      this.storage.push(item);
+      this.pointer++;
+    } else if (this.pointer === this.size) {
+      console.log("Storage full! Cannot enqueue item!");
+    }
   }
 
   dequeue() {
     this.storage.shift();
+    this.pointer--;
+  }
+
+  peek() {
+    console.log(this.storage[0]);
+  }
+
+  isEmpty() {
+    if (this.storage.length > 0) {
+      console.log("Is not empty");
+    } else {
+      console.log("Yes, it is empty");
+    }
+  }
+
+  isFull() {
+    if (this.pointer === this.size) {
+      console.log("It is full");
+    } else if (this.pointer < this.size) {
+      console.log("Not full");
+    }
   }
 
   returnQueue() {
@@ -42,10 +69,13 @@ class Queue {
   }
 }
 
-var myQueue = new Queue();
+var myQueue = new Queue(3);
 console.log(myQueue.storage);
 myQueue.enqueue("A");
+myQueue.peek();
 myQueue.enqueue("B");
 console.log(myQueue.storage);
 myQueue.dequeue();
+console.log(myQueue.storage);
+myQueue.enqueue("B");
 console.log(myQueue.storage);
