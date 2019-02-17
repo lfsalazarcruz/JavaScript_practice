dd429e188e77511e1e6aa579a2c55d56a80eba42
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-02-17 14:42:39 -0800

    Stack Implementation Description.

---
+++ Stack.js
@@ -0,0 +1,9 @@
+// A stack is an abstract data type  with a bounded(predefined) capacity.
+// It is a simple data structure that allows adding and removing elements
+// in a particular order. Every time an element is added, it goes on the top of
+// the stack and the only element that can be removed is the element that is
+// at the top of the stack, just like a pile of objects.
+
+// Last in first out LIFO
+
+var stack = [];
